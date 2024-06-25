ddb7fe7c8f6d28189390ab709d4afb6dc2d90a0b
EugeniaDzadu <jeeniaace@gmail.com>
2024-06-25 14:21:19 +0000

    footers 2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,10 +8,10 @@
                 <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(255, 255, 255, 0.08);">
                     <div class="row g-4">
                         <div class="col-lg-3">
-                            <a href="#" class="d-flex flex-column flex-wrap">
+                            <router-link to="#" class="d-flex flex-column flex-wrap">
                                 <p class="text-white mb-0 display-6">Newsers</p>
                                 <small class="text-light" style="letter-spacing: 11px; line-height: 0;">Newspaper</small>
-                            </a>
+                            </router-link>
                         </div>
                         <div class="col-lg-9">
                             <div class="d-flex position-relative rounded-pill overflow-hidden">
@@ -29,10 +29,10 @@
                             <p class="text-secondary line-h">Email: <span class="text-white">Example@gmail.com</span></p>
                             <p class="text-secondary line-h">Phone: <span class="text-white">+0123 4567 8910</span></p>
                             <div class="d-flex line-h">
-                                <a class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-twitter text-dark"></i></a>
-                                <a class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-facebook-f text-dark"></i></a>
-                                <a class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-youtube text-dark"></i></a>
-                                <a class="btn btn-light btn-md-square rounded-circle" href=""><i class="fab fa-linkedin-in text-dark"></i></a>
+                                <router-link class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-twitter text-dark"></i></router-link>
+                                <router-link class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-facebook-f text-dark"></i></router-link>
+                                <router-link class="btn btn-light me-2 btn-md-square rounded-circle" href=""><i class="fab fa-youtube text-dark"></i></router-link>
+                                <router-link class="btn btn-light btn-md-square rounded-circle" href=""><i class="fab fa-linkedin-in text-dark"></i></router-link>
                             </div>
                         </div>
                     </div>
@@ -40,48 +40,48 @@
                         <div class="footer-item-2">
                             <div class="d-flex flex-column mb-4">
                                 <h4 class="mb-4 text-white">Recent Posts</h4>
-                                <a href="#">
+                                <router-link to="#">
                                     <div class="d-flex align-items-center">
                                         <div class="rounded-circle border border-2 border-primary overflow-hidden">
                                             <img src="/img/footer-1.jpg" class="img-zoomin img-fluid rounded-circle w-100" alt="">
                                         </div>
                                         <div class="d-flex flex-column ps-4">
                                             <p class="text-uppercase text-white mb-3">Life Style</p>
-                                            <a href="#" class="h6 text-white">
+                                            <router-link to="#" class="h6 text-white">
                                                 Get the best speak market, news.
-                                            </a>
+                                            </router-link>
                                             <small class="text-white d-block"><i class="fas fa-calendar-alt me-1"></i> Dec 9, 2024</small>
                                         </div>
                                     </div>
-                                </a>
+                                </router-link>
                             </div>
                             <div class="d-flex flex-column">
-                                <a href="#">
+                                <router-link to="#">
                                     <div class="d-flex align-items-center">
                                         <div class="rounded-circle border border-2 border-primary overflow-hidden">
                                             <img src="/img/footer-2.jpg" class="img-zoominimg-fluid rounded-circle w-100" alt="">
                                         </div>
                                         <div class="d-flex flex-column ps-4">
                                             <p class="text-uppercase text-white mb-3">Sports</p>
-                                            <a href="#" class="h6 text-white">
+                                            <router-link to="#" class="h6 text-white">
                                                 Get the best speak market, news.
-                                            </a>
+                                            </router-link>
                                             <small class="text-white d-block"><i class="fas fa-calendar-alt me-1"></i> Dec 9, 2024</small>
                                         </div>
                                     </div>
-                                </a>
+                                </router-link>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6 col-xl-3">
                         <div class="d-flex flex-column text-start footer-item-3">
                             <h4 class="mb-4 text-white">Categories</h4>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Sports</a>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Magazine</a>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Lifestyle</a>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Politician</a>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Technology</a>
-                            <a class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Intertainment</a>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Sports</router-link>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Magazine</router-link>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Lifestyle</router-link>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Politician</router-link>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Technology</router-link>
+                            <router-link class="btn-link text-white" href=""><i class="fas fa-angle-right text-white me-2"></i> Intertainment</router-link>
                         </div>
                     </div>
                     <div class="col-lg-6 col-xl-3">
@@ -132,13 +132,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>Your Site Name</a>, All right reserved.</span>
+                        <span class="text-light"><router-link to="#"><i class="fas fa-copyright text-light me-2"></i>Your Site Name</router-link>, All right reserved.</span>
                     </div>
                     <div class="col-md-6 my-auto text-center text-md-end text-white">
                         <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
                         <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
                         <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a>
+                        Designed By <router-link class="border-bottom" href="https://htmlcodex.com">HTML Codex</router-link> Distributed By <router-link to="https://themewagon.com">ThemeWagon</router-link>
                     </div>
                 </div>
             </div>
@@ -147,7 +147,7 @@
 
 
         <!-- Back to Top -->
-        <a href="#" class="btn btn-primary border-2 border-white rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
+        <router-link to="#" class="btn btn-primary border-2 border-white rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></router-link>   
 
         
         
