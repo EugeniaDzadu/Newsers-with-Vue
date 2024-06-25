8bb9fbbea2f4426c68437b8a3e366519141a6372
EugeniaDzadu <jeeniaace@gmail.com>
2024-06-25 00:52:16 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    
+
+    <div class="wrapper">
+      <h1>hi</h1>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
